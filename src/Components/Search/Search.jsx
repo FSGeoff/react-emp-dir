9ca7692bef0cbc390e85aba99058dc369a174f48
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-19 21:07:22 -0500

    initial files, all routes working

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+           <h1>This is my Search Page</h1>
+        );
+    }
+}
+
+export default Search;
