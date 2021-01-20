166b70f97535cbe06a68526dcaf4390df4f2fe47
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-20 10:00:14 -0500

    added directory card

--- src/Components/Directory/Directory.jsx
+++ src/Components/Directory/Directory.jsx
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Directory extends Component {
+
+state = {
+    bioPic: "",
+    name: "",
+    email: "",
+    location: "",
+    username: ""
+}
+
+componentWillMount() {
+    this.getEmployee();
+}
+
 	getEmployee = () => {
 		axios
 			.get("https://randomuser.me/api/")
@@ -16,9 +29,19 @@ class Directory extends Component {
 				console.log(employeeArr[0].email);
 				console.log(employeeArr[0].login.username);
 				console.log(employeeArr[0].login.password);
-				console.log(employeeArr[0].picture.large);
+				console.log(employeeArr[0].picture.thumbnail);
 				this.setState({
-					imageUrl: response.data.results,
+                    bioPic: employeeArr[0].picture.thumbnail,
+                    name:
+						" " +
+						employeeArr[0].name.title +
+						" " +
+						employeeArr[0].name.first +
+						" " +
+                        employeeArr[0].name.last,
+                        email: employeeArr[0].email,
+                        location: employeeArr[0].location.country,
+                        username: employeeArr[0].login.username
 				});
 			})
 			.catch((err) => {
@@ -30,15 +53,15 @@ class Directory extends Component {
 		this.getEmployee();
 	}
 	render() {
-		const boxStyle = {
-			outline: "solid 1px black",
-			fontSize: "18px",
-			fontFamily: "sans-serif",
-			backgroundColor: "#AED1EF",
-			padding: "10px",
-			border: "5px",
-			margin: "auto",
-		};
+		// const boxStyle = {
+		// 	outline: "solid 1px black",
+		// 	fontSize: "18px",
+		// 	fontFamily: "sans-serif",
+		// 	backgroundColor: "#AED1EF",
+		// 	padding: "10px",
+		// 	border: "5px",
+		// 	margin: "auto",
+		// };
 		const headerStyle = {
 			fontSize: "40px",
 			fontFamily: "Georgia, serif",
@@ -48,59 +71,70 @@ class Directory extends Component {
 			margin: "8px",
 			border: "8px",
 			padding: "8px",
-        };
-        
-        const navText = {
-            fontSize: "25px",
-            fontFamily: "Georgia, serif",
-        }
+		};
+
+		const navText = {
+			fontSize: "25px",
+			fontFamily: "Georgia, serif",
+		};
 
 		return (
 			<>
-				<body style={{ backgroundColor: "#052E52" }}>
-					<nav
-						className="navbar is-black"
-						role="navigation"
-						aria-label="main navigation"
-					>
-						<div className="navbar-brand">
-							<img
-								src="https://png.pngtree.com/png-vector/20190316/ourlarge/pngtree-employee-icon-design-template-vector-isolated-png-image_856368.jpg"
-								style={{ height: "125px", width: "125px" }}
-								alt="employee icon"
-							/>
-						</div>
+				<nav
+					className="navbar is-black"
+					role="navigation"
+					aria-label="main navigation"
+				>
+					<div className="navbar-brand">
+						<img
+							src="https://png.pngtree.com/png-vector/20190316/ourlarge/pngtree-employee-icon-design-template-vector-isolated-png-image_856368.jpg"
+							style={{ height: "125px", width: "125px" }}
+							alt="employee icon"
+						/>
+					</div>
 
-						<div id="navbarBasicExample" className="navbar-menu">
-							<div className="navbar-start">
-								<a className="navbar-item" href="/" style={navText}>Search</a>
+					<div id="navbarBasicExample" className="navbar-menu">
+						<div className="navbar-start">
+							<a className="navbar-item" href="/" style={navText}>
+								Search
+							</a>
 
-								<a className="navbar-item" href="/" style={navText}>Directory</a>
-							</div>
-						</div>
-					</nav>
-					<header>
-						<h1 style={headerStyle}>EMPLOYEE DIRECTORY</h1>
-					</header>
-
-					<div className="columns">
-						<div className="column is-3" style={boxStyle}>
-							<figure class="image is-128x128 is-centered">
-								<img alt="bio-pic"
-									className="is-rounded"
-									src="https://bulma.io/images/placeholders/128x128.png"
-								/>
-							</figure>
-							<p1>Name:</p1>
-							<br />
-							<p1>Location:</p1>
-							<br />
-							<p1>Email:</p1>
-							<br />
-							<p1>Username:</p1>
+							<a className="navbar-item" href="/" style={navText}>
+								Directory
+							</a>
 						</div>
 					</div>
-				</body>
+				</nav>
+				<header>
+					<h1 style={headerStyle}>EMPLOYEE DIRECTORY</h1>
+				</header>
+                <div className="is-12" style={{ height: "30px" }}></div>
+            <div className="columns">
+                <div className="column is-3" style={{border: "solid 1px black"}}>
+                <div className="card">
+  <div className="card-image">
+   
+  </div>
+  <div className="card-content">
+    <div className="media">
+      <div className="media-left">
+        <figure className="image is-48x48">
+          <img src={this.state.bioPic} alt="bio pic"/>
+        </figure>
+      </div>
+      <div className="media-content">
+        <p className="title is-4">{this.state.name}</p>
+        <p className="subtitle is-6">{this.state.email}</p>
+        <p>{this.state.location}</p>
+        <p>{this.state.username}</p>
+      </div>
+    </div>
+
+   
+  </div>
+</div>
+                </div>
+            </div>
 			</>
 		);
 	}
