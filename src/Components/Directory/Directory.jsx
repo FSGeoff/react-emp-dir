b44add8ff821f8baea0d37a7505d3cf998b260b3
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-19 21:55:03 -0500

    added api call

--- src/Components/Directory/Directory.jsx
+++ src/Components/Directory/Directory.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Directory extends Component {
-    render() {
-        return (
-           <h1>This is my directory Page</h1>
-        );
-    }
+	getEmployee = () => {
+		axios
+			.get("https://randomuser.me/api/")
+			.then((response) => {
+				console.log(response.data.results);
+				this.setState({
+					imageUrl: response.data.results,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	componentDidMount() {
+		this.getEmployee();
+	}
+	render() {
+		return <h1>Directory Page</h1>;
+	}
 }
 
 export default Directory;
